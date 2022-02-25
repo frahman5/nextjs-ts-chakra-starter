62499a7a5fe3f5d02f8f1a6e77c3fc90d255e750
Faiyam Rahman <faiyam@faiyamrahman.com>
2022-02-24 19:17:01 -0500

    stripped bare starter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "10.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "3.75rem",
+    "7xl": "4.5rem",
+    "8xl": "6rem",
+    "9xl": "8rem",
+  },
+});
 
 function App({ Component, pageProps }: AppProps): React.ReactNode {
   return (
-    <ThemeProvider>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ChakraProvider>
+      <Heading as="h1" fontSize="4xl">
+        Investing in Technologies of Wellbeing
+      </Heading>
+      {/* <Component {...pageProps} /> */}
+    </ChakraProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,33 @@
 import Layout from "../components/Layout";
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  // <Layout title="Home | Next.js + TypeScript Example">
+  <Box>
     <Hero />
-  </Layout>
+  </Box>
+  // {/* </Layout> */}
 );
 
 const Hero = () => {
   return (
-    <Box>
-      <p>Hero placeholder</p>
+    <Box bg="tomato" w="100%" p={4} color="white">
+      {/* 
+      h1 should be 40px size, 35px line height, weight 400, font Fedra Serif A Std
+      On Mobile, that changes to: 
+        font size: 25px
+        Line height: 35px
+      
+      */}
+      <Text size="3xl">Investing in Technologies of Wellbeing</Text>{" "}
+      {/* 
+      h4 should be 425px size, 27.5px line height, weight 400, font Fedra Serif A Std
+      On Mobile, that changes to: 
+        font size: 14px
+        Line height: 20px
+      
+      */}
+      <Heading as="h4">ISHA YOGA CENTER, CALIFORNIA</Heading>
     </Box>
   );
 };
